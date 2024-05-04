71ca5830d7eed002fd79c6cbd8156619edeefd9e
Miralhas <victor2.miralhas@gmail.com>
2024-05-04 10:33:38 -0300

    feature: implement constraint '@Positive' on 'maximumPower' & 'efficiency' columns in SolarPanel Entity

--- src/main/java/fatec/sp/gov/br/smartleaf/domain/model/SolarPanel.java
+++ src/main/java/fatec/sp/gov/br/smartleaf/domain/model/SolarPanel.java
@@ -3,8 +3,10 @@ package fatec.sp.gov.br.smartleaf.domain.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -29,10 +31,12 @@ public class SolarPanel {
     @Column(name = "model", nullable = false)
     private String model;
 
+    @Positive
     @NotNull
     @Column(name = "maximum_power", nullable = false)
     private Integer maximumPower;
 
+    @Positive
     @NotNull
     @Column(name = "efficiency", nullable = false)
     private Integer efficiency;
